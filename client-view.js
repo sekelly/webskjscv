acac677c3b77db4da3b6dd01100d4e7ceb647bee
SE Kelly <94656500+sekelly@users.noreply.github.com>
2023-03-12 16:10:14 -0700

    Update client-view.js
    changed to kmagaoay

--- client-view.js
+++ client-view.js
@@ -8,13 +8,13 @@ ZoomMtg.i18n.reload('en-US')
 
 // setup your signature endpoint here: https://github.com/zoom/meetingsdk-sample-signature-node.js
 var signatureEndpoint = 'https://ybami8c770.execute-api.us-east-1.amazonaws.com/latest'
-var sdkKey = 'XgkIvvHHyAK9na3ZPQVuB8fSU8T3kKghPSWR'
-var meetingNumber = '99223103673'
+var sdkKey = 'EStDoqGOS6NYeFGHIX_qA'
+var meetingNumber = '97150709574'
 var role = 1
-var leaveUrl = 'https://zoom.us/meeting/tJ0vdeqprjgsHddmdQLBrVkBbjFYs3YyXnRj/postsurvey?tk=JrE89kjj9zfg3UyB3AkUGEzQgI4ZIAGQSNrExqtrB7U.DQMAAAAXGihouRYtSllvZXp5S1RfYU5oMENjSTh2SWxRFW1hZG1heEBrZWxtYWdhb2F5LmNvbQdNYWQgTWF4AAAAAAAAAAAAAAAAAAAAAAAAGFV6K3NQTGRjUXpxaW1CdUV2WTJiaGc9PQA'
+var leaveUrl = ''
 var userName = 'ClientView'
 var userEmail = ''
-var passWord = ''
+var passWord = '123'
 // pass in the registrant's token if your meeting or webinar requires registration. More info here:
 // Meetings: https://marketplace.zoom.us/docs/sdk/native-sdks/web/client-view/meetings#join-meeting-with-registration-required
 // Webinars: https://marketplace.zoom.us/docs/sdk/native-sdks/web/client-view/webinars#join-webinar-with-registration-required
